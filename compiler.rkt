1053523d03d0f0071a5102741fdbc70ff90a42fa
haskellstudio <haskellstudio@gmail.com>
2017-05-08 10:19:04 +0800

    1

--- compiler.rkt
+++ compiler.rkt
@@ -43,3 +43,109 @@
      ("partial evaluator" ,pe-arith ,interp-scheme)
      ))
 
+
+
+(define ast1.4 `(- 8))
+(define ast1.1 `(+ (read) ,ast1.4))
+
+;(match ast1.1
+ ; [`(,op ,child1 ,child2)
+  ;  (print op)  (newline)
+;
+ ;   
+  ;  (print child1) (newline)
+;
+ ;   (print child2) (newline)
+  ;  ])
+
+(define (leaf? arith)
+  (match arith
+    [(? fixnum?) #t]
+    [`(read) #t]
+    [`(- ,c1) #f]
+    [`(+ ,c1 ,c2) #f])
+    )
+
+
+;(leaf? `(read))
+
+
+
+;(leaf? `(- 8))
+
+;(leaf? `(+ 1 1))
+
+
+
+(define (R0? sexp)
+(match sexp
+[(? fixnum?) #t]
+[`(read) #t]
+[`(- ,e) (R0? e)]
+[`(+ ,e1 ,e2)
+(and (R0? e1) (R0? e2))]
+[`(program ,e) (R0? e)]
+[else #f]))
+
+
+
+;(R0? `(+ (read) (- 8)))
+;(R0? `(- (read) (+ 8)))
+
+
+;(interp-R0 ast1.1)
+
+(define (interp-R0 e)
+  (match e
+    [ (? fixnum?) e]
+    [`(read)
+     (let [ (r (read))]
+       (cond [ (fixnum? r) r]
+             [else (error `interp-R0 "input not an integer")]))]
+    [`(- ,(app interp-R0 v1) ,(app interp-R0 v2))
+     (fx- v1 v2)]
+
+    [`(+ ,(app interp-R0 v1) ,(app interp-R0 v2))
+     (fx+ v1 v2)]
+
+    [`(program ,(app interp-R0 v)) v]
+    ))
+
+
+
+
+(define (pe-arith_ e)
+  (match e
+    [(? fixnum?) e]
+    [`(read) `(read)]
+    [`(- ,(app pe-arith_ r1))   (cond [(fixnum? r1) (fx- 0 r1)]
+                                      [else '(- ,r1)])]
+    [`(+ ,(app pe-arith_ r1) ,(app pe-arith_ r2)) (cond
+                                                    [(fixnum? r1)
+                                                     (match r2
+                                                       [(? fixnum?) (+ r1 r2)]
+                                                       [`(read) `(+ ,r1 (read))]
+                                                       [`(+ ,(app pe-arith_ s1) ,(app pe-arith_ s2))
+                                                        (cond [(and (fixnum? s1) (fixnum? s2))     (+ r1 (+ s1 s2))])
+                                                        (cond [(fixnum? s1) '(+ ,(+ r1 s1) ,s2) ])
+                                                        (cond [(fixnum? s2) `(+ ,(+ r1 s2) ,s1)])]
+                                                       )]
+                                                    [(fixnum? r2)
+                                                     (match r1
+                                                       [(? fixnum?) (+ r1 r2)]
+                                                       [`(read) `(+  (read) ,r2)]
+                                                       [`(+ ,(app pe-arith_ s1) ,(app pe-arith_ s2))
+                                                        (cond [(and (fixnum? s1) (fixnum? s2))     (+  (+ s1 s2) r1)])
+                                                        (cond [(fixnum? s1) `(+ ,s2 ,(+ r1 s1))  ])
+                                                        (cond [(fixnum? s2) `(+ ,s1 ,(+ r1 s2))])]
+                                                       )]
+                                                    [(and (fixnum? r1) (fixnum? r2)) (fx+ r1 r2)]
+                                                    [else `(+ ,r1 ,r2)]) ]
+    ))
+
+
+;(pe-arith_ '(+ 1 (+ (read) 2)))
+(pe-arith_ '(+ (+ (read) 1) 2))
+
+
+
